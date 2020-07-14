0ff73d70fd48ad92d7ed6db73f375eb855cc47d3
Ryan Kittle <ryan.h.kittle@gmail.com>
2020-07-13 17:26:41 -0700

    OnCreate, OnResume, and OnPause functions implemented. Errors are included because SnakeEngine class still needs to be created.

--- app/src/main/java/com/example/snake/MainActivity.java
+++ app/src/main/java/com/example/snake/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.snake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
+    SnakeEngine snakeEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Gets the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+        // Then save the values as a Point object, ie size
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create instance of the SnakeEngine class using
+        // this context and the screen dimensions
+        snakeEngine = new SnakeEngine(this, size);
+
+        // Set the instance as the view
+        setContentView(snakeEngine);
+    }
+
+    // Resume the process when app re-opens
+    @Override
+    protected void onResume() {
+        super.onResume();
+        snakeEngine.resume();
+    }
+    // Stop the process when the app closes
+    @Override
+    protected void onPause() {
+        super.onPause();
+        snakeEngine.pause();
     }
 }
